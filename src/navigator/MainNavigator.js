6ea9baa18f1aa219cf52be03b25bd5fccfb242ec
crazynavi <117359800+TrustedSeven@users.noreply.github.com>
2023-01-25 01:48:27 -0600

    delete unused screens

--- src/navigator/MainNavigator.js
+++ src/navigator/MainNavigator.js
@@ -2,17 +2,12 @@ import React, { useState, useEffect, useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { AuthContext } from '../AuthProvider';
-import LogInScreen from '../container/login';
-import RegisterScreen from '../container/register';
 import SuccessScreen from '../container/success';
 
 const Stack = createNativeStackNavigator();
 
 const MainNavigator = ({ parentNavigator }) => {
-    const { userProfile } = useContext(AuthContext);
-    const {config} = useContext(AuthContext);
-
+    
     return (
         <NavigationContainer>
             <>
@@ -20,20 +15,13 @@ const MainNavigator = ({ parentNavigator }) => {
                     initialRouteName={'Login'} 
                     screenOptions={{ headerShown: false }}
                 >
-                    {
-                        userProfile ?
-                            (
+                    
+                            
                                 <>
                                 <Stack.Screen name="Success" component={SuccessScreen} />
                                 </>
-                            ) : (
-                                <>
-                                    {/* <Stack.Screen name="LogIn" component={LogInScreen} />
-                                    <Stack.Screen name="Register" component={RegisterScreen} /> */}
-                                    <Stack.Screen name="Success" component={SuccessScreen} />
-                                </>
-                            )
-                    }
+                            
+                    
                 </Stack.Navigator>
             </>
         </NavigationContainer>
